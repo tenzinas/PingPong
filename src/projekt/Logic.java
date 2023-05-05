aa0027d930b94c091e95b1d819c0cc504234a4b7
ISA <islambekturaisov@gmail.com>
2023-05-06 02:53:15 +0300

    добавление стены вместо биты . исправление ошибок . начать игру с помощью пробела space

--- src/projekt/Logic.java
+++ src/projekt/Logic.java
@@ -1,24 +1,26 @@
-package projekt;
-
+package project1;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.util.concurrent.TimeUnit;
 
-    public class Logic extends JPanel implements ActionListener, KeyListener {
+public class Logic extends JPanel implements ActionListener, KeyListener {
     public static volatile int Bit1Y = 120;
     private int Bit2Y = 120;
     private int BallPX = 283, BallPY = 155;
-    private int BallX = 2, BallY = 3;
+    private int BallX = 2, BallY = 2;
+
+    private int score = 0 ;
+    JLabel scoree = new JLabel(String.valueOf(score));
     private Timer time;
 
     public Logic() {
         addKeyListener(this);
         time = new Timer(10, this);
         setFocusable(true);
-        time.start();
     }
 
     @Override
@@ -28,10 +30,10 @@ import java.awt.event.KeyListener;
         g.setColor(Color.white);
         g.drawLine(290, 0, 290, 400);
         g.fillRect(0, Bit1Y, 10, 100);
-        g.fillRect(572, Bit2Y, 10, 100);
+        g.fillRect(572, 0, 10, 400);
         g.fillRect(BallPX, BallPY, 14, 14);
     }
-
+    int BallConst = BallY;
     @Override
     public void actionPerformed(ActionEvent e) {
         repaint();
@@ -46,7 +48,7 @@ import java.awt.event.KeyListener;
         if (new Rectangle(BallPX, BallPY, 14, 14).intersects(new Rectangle(0, Bit1Y, 10, 100))) {
             BallX = -BallX;
         }
-        if (new Rectangle(BallPX, BallPY, 14, 14).intersects(new Rectangle(572, Bit2Y, 10, 100))) {
+        if (new Rectangle(BallPX, BallPY, 14, 14).intersects(new Rectangle(572, 0, 10, 400))) {
             BallX = -BallX;
         }
     }
@@ -58,18 +60,29 @@ import java.awt.event.KeyListener;
 
     @Override
     public void keyPressed(KeyEvent e) {
-        System.out.println(e.getKeyCode());
         if (e.getKeyCode() == 40) {
-            Bit1Y += 5;
+            System.out.println(0);
+            Bit1Y += 10;
+            if(Bit1Y >= 262){
+                Bit1Y = 262 ;
+            }
 
         }
         if (e.getKeyCode() == 38) {
-            Bit1Y -= 5;
+            System.out.println(1);
+            Bit1Y -= 10;
+            if(Bit1Y <= 0){
+                Bit1Y = 0 ;
+            }
 
         }
     }
 
     @Override
     public void keyReleased(KeyEvent e) {
+        if(e.getKeyCode() == 32){
+            time.start();
+            System.out.println(2);
+        }
     }
 }
